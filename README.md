52679c202e40efad829ccbb7b1abc1552133fd7a
CaoiveMcTigue <keymctigue92@gmail.com>
2021-07-26 14:12:04 +0000

    working on the readme.md file for battleship game. outlined my features, testing, technology and credits

--- README.md
+++ README.md
@@ -1,104 +1,38 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# Read me for Battleship Game
 
-Welcome CaoiveMcTigue,
+## Introduction
+The sole purpose for this python project was following a brief layout for my third milestone project, building a battleship game using python was suggested as an idea within the brief therefore I decided to give it a go.<br>
+Using a game, I feel is a fun and interactive way to display my knowledge in python.
 
-This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Gitpod and the extensions we use. The last update to this file was: **July 2, 2021**
+## Features
+My battleship game, will be displayed in a 10x10 grid. It is a one player game. There will be 8 ships of variable lengths, randomly placed around the grid by the computer. The user will start with 50 bullets to take down the ships. The user must choose a row and column of where they would like to shot, the row must be picked by a letter from the range A-J and the column is picked using a number from the range 0-9, they must be typed into the terminal together such as A3. This will indicate exactly where the user would like to shot on the grid.<br>
+For every shot that shots or misses, it will be shown on the grid using a symbol from a legend explained below and outlined in a comment in the code for this game.<br>
+A ship cannot be placed diagonally in this battleships game. Therefore when a shot hits, the rest of that ship can only be in one of four directions, left, right, up, or down.<br>
+To win this battleship game, all 8 ships must be fully unearthed before running out of bullets. If you have used all 50 bullets and have not found all eight ships, you lose.<br>
+The legend in this game is as follows:<br>
+"." = empty space<br>
+"O" = part of the ship (unhit)<br>
+"X" = part of the ship hit with a bullet<br>
+"#" = empty space hit with a bullet (a miss because it hit no ship)<br>
+## Testing
+To test that this python battleships game was working correctly, I first added a function to my code called debug_mode. This debug_mode is equal to false when the game is being played but to test that all functions in my code were working correctly I set this equal to true (debug_mode = True). What this did is aloud me to see where the computer had placed the ships using the symbol from the above legend of "O", when these symbol were visible I could hit and miss while playing the game to ensure the correct message was being displayed in these situations.<br>
+I also tested the python using
+## Technology
+* Python was the main language used in this battleships game. An extension of python had to be installed for it to be used correctly.<br>
+* I used GitHub to store my code
+* I used GitPod to write my code
+## Deployment
+## Credits
+As always I relied heavily on my code institute [course notes.](https://discuss.codecademy.com/t/excellent-battleship-game-written-in-python/430605)<br>I also used a number of websites and Youtube videos when building this battleships game in python these include:<br>
+### Youtube Video
+* [freeCodeCamp.org](https://www.youtube.com/watch?v=rfscVS0vtbw&t=222s)
+* [Programming with Mosh](https://www.youtube.com/watch?v=_uQrJ0TkZlc)
+* [freeCodeCamp.org](https://www.youtube.com/watch?v=8ext9G7xspg&t=2134s)
+* [Learn Python with CodeCademy: Battleship!](https://www.youtube.com/watch?v=7Ki_2gr0rsE&t=883s)
+* [Robert Heaton](https://www.youtube.com/channel/UCOp0YjvVlwp-GfdpgX0V3sA)
+* [CS Students](https://www.youtube.com/watch?v=MgJBgnsDcF0)
+### Websites
+* [w3school](https://www.w3schools.com/python/default.asp)
+* [stackoverflow](https://stackoverflow.com/questions/tagged/python)
+* [CodeCademy](https://discuss.codecademy.com/t/excellent-battleship-game-written-in-python/430605)
 
-## Gitpod Reminders
-
-To run a frontend (HTML, CSS, Javascript only) application in Gitpod, in the terminal, type:
-
-`python3 -m http.server`
-
-A blue button should appear to click: _Make Public_,
-
-Another blue button should appear to click: _Open Browser_.
-
-To run a backend Python file, type `python3 app.py`, if your Python file is named `app.py` of course.
-
-A blue button should appear to click: _Make Public_,
-
-Another blue button should appear to click: _Open Browser_.
-
-In Gitpod you have superuser security privileges by default. Therefore you do not need to use the `sudo` (superuser do) command in the bash terminal in any of the lessons.
-
-To log into the Heroku toolbelt CLI:
-
-1. Log in to your Heroku account and go to *Account Settings* in the menu under your avatar.
-2. Scroll down to the *API Key* and click *Reveal*
-3. Copy the key
-4. In Gitpod, from the terminal, run `heroku_config`
-5. Paste in your API key when asked
-
-You can now use the `heroku` CLI program - try running `heroku apps` to confirm it works. This API key is unique and private to you so do not share it. If you accidentally make it public then you can create a new one with _Regenerate API Key_.
-
-------
-
-## Release History
-
-We continually tweak and adjust this template to help give you the best experience. Here is the version history:
-
-**July 19 2021:** Remove `font_fix` script now that the terminal font issue is fixed.
-
-**July 2 2021:** Remove extensions that are not available in Open VSX.
-
-**June 30 2021:** Combined the P4 and P5 templates into one file, added the uptime script. See the FAQ at the end of this file.
-
-**June 10 2021:** Added: `font_fix` script and alias to fix the Terminal font issue
-
-**May 10 2021:** Added `heroku_config` script to allow Heroku API key to be stored as an environment variable.
-
-**April 7 2021:** Upgraded the template for VS Code instead of Theia.
-
-**October 21 2020:** Versions of the HTMLHint, Prettier, Bootstrap4 CDN and Auto Close extensions updated. The Python extension needs to stay the same version for now.
-
-**October 08 2020:** Additional large Gitpod files (`core.mongo*` and `core.python*`) are now hidden in the Explorer, and have been added to the `.gitignore` by default.
-
-**September 22 2020:** Gitpod occasionally creates large `core.Microsoft` files. These are now hidden in the Explorer. A `.gitignore` file has been created to make sure these files will not be committed, along with other common files.
-
-**April 16 2020:** The template now automatically installs MySQL instead of relying on the Gitpod MySQL image. The message about a Python linter not being installed has been dealt with, and the set-up files are now hidden in the Gitpod file explorer.
-
-**April 13 2020:** Added the _Prettier_ code beautifier extension instead of the code formatter built-in to Gitpod.
-
-**February 2020:** The initialisation files now _do not_ auto-delete. They will remain in your project. You can safely ignore them. They just make sure that your workspace is configured correctly each time you open it. It will also prevent the Gitpod configuration popup from appearing.
-
-**December 2019:** Added Eventyret's Bootstrap 4 extension. Type `!bscdn` in a HTML file to add the Bootstrap boilerplate. Check out the <a href="https://github.com/Eventyret/vscode-bcdn" target="_blank">README.md file at the official repo</a> for more options.
-
-------
-
-## FAQ about the uptime script
-
-**Why have you added this script?**
-
-It will help us to calculate how many running workspaces there are at any one time, which greatly helps us with cost and capacity planning. It will help us decide on the future direction of our cloud-based IDE strategy.
-
-**How will this affect me?**
-
-For everyday usage of Gitpod, it doesn’t have any effect at all. The script only captures the following data:
-
-- An ID that is randomly generated each time the workspace is started.
-- The current date and time
-- The workspace status of “started” or “running”, which is sent every 5 minutes.
-
-It is not possible for us or anyone else to trace the random ID back to an individual, and no personal data is being captured. It will not slow down the workspace or affect your work.
-
-**So….?**
-
-We want to tell you this so that we are being completely transparent about the data we collect and what we do with it.
-
-**Can I opt out?**
-
-Yes, you can. Since no personally identifiable information is being captured, we'd appreciate it if you let the script run; however if you are unhappy with the idea, simply run the following commands from the terminal window after creating the workspace, and this will remove the uptime script:
-
-```
-pkill uptime.sh
-rm .vscode/uptime.sh
-```
-
-**Anything more?**
-
-Yes! We'd strongly encourage you to look at the source code of the `uptime.sh` file so that you know what it's doing. As future software developers, it will be great practice to see how these shell scripts work.
-
----
-
-Happy coding!
--- main.py
+++ main.py
@@ -9,10 +9,10 @@ Legend:
 4. "#" = empty space that was shot with a bullet, a miss because it hit no ship
 """
 
-grid = [[]]  #  variable for grid
-grid_size = 10  #  variable for grid size
-num_ships = 8  #  variable for number of ships to place
-bullets_left = 50  # variable for bullets left
+grid = [[]]  #variable for grid
+grid_size = 10  #variable for grid size
+num_ships = 8  #variable for number of ships to place
+bullets_left = 50  #variable for bullets left
 game_over = False #variable for game over
 sunk_ships = 0 #variable for number of ships sunk
 ship_positions = [[]] #variable for ship positions
@@ -20,218 +20,218 @@ alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" #variable for alphabet
 
 def validate_grid_and_place_ship(start_row, end_row, start_col, end_col):
     #check to see if its safe to place a ship in certain row or column.
-    global grid
-    global ship_positions
+global grid
+global ship_positions
 
-    all_valid = True
-    for r in range(start_row, end_row):
-        for c in range(start_col, end_col):
-            if grid[r][c] != ".":
-                all_valid = False
-                break
-    if all_valid:
-        ship_positions.append([start_row, end_row, start_col, end_col])
-        for r in range(start_row, end_row):
-            for c in range(start_col, end_col):
-                grid[r][c] = "O"
-    return all_valid
+all_valid = True
+for r in range(start_row, end_row):
+for c in range(start_col, end_col):
+    if grid[r][c] != ".":
+        all_valid = False
+        break
+if all_valid:
+ship_positions.append([start_row, end_row, start_col, end_col])
+for r in range(start_row, end_row):
+    for c in range(start_col, end_col):
+        grid[r][c] = "O"
+return all_valid
 
 
 def try_to_place_ship(row, col, direction, length):
-    """Based on direction will use above function to help in trying to place a ship on the grid.
-        Returns validate_grid_and_place_ship which will either be true or false."""
-    global grid_size
+"""Based on direction will use above function to help in trying to place a ship on the grid.
+Returns validate_grid_and_place_ship which will either be true or false."""
+global grid_size
 
-    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
-    if direction == "left":
-        if col - length < 0:
-            return False
-        start_col = col - length + 1
-    
-    elif direction == "right":
-        if col + length >= grid_size:
-            return False
-        end_col = col + length
-    
-    elif direction == "up":
-        if row - length < 0:
-            return False
-        start_row = row - length + 1
-    
-    elif direction == "down":
-        if row + length >= grid_size:
-            return False
-        end_row = row + length
+start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+if direction == "left":
+if col - length < 0:
+    return False
+start_col = col - length + 1
 
-    return validate_grid_and_place_ship(start_row, end_row, start_col, end_col)
+elif direction == "right":
+if col + length >= grid_size:
+    return False
+end_col = col + length
+
+elif direction == "up":
+if row - length < 0:
+    return False
+start_row = row - length + 1
+
+elif direction == "down":
+if row + length >= grid_size:
+    return False
+end_row = row + length
+
+return validate_grid_and_place_ship(start_row, end_row, start_col, end_col)
 
 def create_grid():
-    #will create a 10x10 grid and randomly place down ships
-    global grid
-    global grid_size
-    global num_ships
-    global ship_positions
+#will create a 10x10 grid and randomly place down ships
+global grid
+global grid_size
+global num_ships
+global ship_positions
 
-    random.seed(time.time())
+random.seed(time.time())
 
-    rows, cols = (grid_size, grid_size)
+rows, cols = (grid_size, grid_size)
 
-    grid = []
-    for r in range(rows):
-        row = []
-        for c in range(cols):
-            row.append(".")
-        grid.append(row)
-    
-    num_of_ships_placed = 0
+grid = []
+for r in range(rows):
+row = []
+for c in range(cols):
+    row.append(".")
+grid.append(row)
 
-    ship_positions = []
+num_of_ships_placed = 0
 
-    while num_of_ships_placed != num_ships:
-        random_row = random.randint(0, rows - 1)
-        random_col = random.randint(0, cols - 1)
-        direction = random.choice(["left", "right", "up", "dowm"])
-        ship_size = random.randint(3, 5)
-        if try_to_place_ship(random_row, random_col, direction, ship_size):
-            num_of_ships_placed += 1
+ship_positions = []
+
+while num_of_ships_placed != num_ships:
+random_row = random.randint(0, rows - 1)
+random_col = random.randint(0, cols - 1)
+direction = random.choice(["left", "right", "up", "dowm"])
+ship_size = random.randint(3, 5)
+if try_to_place_ship(random_row, random_col, direction, ship_size):
+    num_of_ships_placed += 1
 
 def print_grid():
-    #will print grid with rows A-J and colums 0-9
-    global grid
-    global alphabet
+#will print grid with rows A-J and colums 0-9
+global grid
+global alphabet
 
-    debug_mode = True 
+debug_mode = True 
 
-    alphabet = alphabet(0: len(grid) + 1)
+alphabet = alphabet(0: len(grid) + 1)
 
-    for row in range (len(grid)):
-        print(alphabet[row], end=") ")
-        for col in range (len(grid[row])):
-            if grid[row][col] == "O":
-                if debug_mode:
-                    print("O", end=" ")
-                else:
-                    print(".", end=" ")
-            else:
-                print(grid[row][col], end=" ")
-        print("")
-    
-    print(" ", end=" ")
-    for i in range(len(grid[0])):
-        print(str(i), end=" ")
-    print("")
+for row in range (len(grid)):
+print(alphabet[row], end=") ")
+for col in range (len(grid[row])):
+    if grid[row][col] == "O":
+        if debug_mode:
+            print("O", end=" ")
+        else:
+            print(".", end=" ")
+    else:
+        print(grid[row][col], end=" ")
+print("")
+
+print(" ", end=" ")
+for i in range(len(grid[0])):
+print(str(i), end=" ")
+print("")
 
 def valid_bullet_placement():
-        #will get valid row and column to place bullet shot
-        global alphabet
-        global grid
+#will get valid row and column to place bullet shot
+global alphabet
+global grid
 
-        valid_placement = False
-        row = -1
-        col = -1
-        while valid_placement is False:
-            placement = input("Enter row (A-J) and column (0-9) such as A3: ")
-            placement = placement.upper()
-            if len(placement) <= 0 or len(placement) > 2:
-                print("Error: Please enter only one row and column")
-                continue
-            row = placement[0]
-            col = placement[1]
-            if not row.isalpha() or not col.isnumeric():
-                print("Error: Please enter letter (A-J) for row and (0-9) for column")
-                continue
-            row = alphabet.find(row)
-            if not (-1 < row < grid_size):
-                print("Error: Please enter letter (A-J) for row and (0-9) for column")
-                continue
-            col = int(col)
-            if not (-1 < col < grid_size):
-                print("Error: Please enter letter (A-J) for row and (0-9) for column")
-                continue
-            if grid[row][col] == "#" or grid[row][col] == "X":
-                print("You have already shot a bullet here, pick somewhere else")
-                continue
-            if grid[row][col] == "." or grid[row][col] == "O":
-                valid_placement = True
-        
-        return row, col
+valid_placement = False
+row = -1
+col = -1
+while valid_placement is False:
+    placement = input("Enter row (A-J) and column (0-9) such as A3: ")
+    placement = placement.upper()
+    if len(placement) <= 0 or len(placement) > 2:
+        print("Error: Please enter only one row and column")
+        continue
+    row = placement[0]
+    col = placement[1]
+    if not row.isalpha() or not col.isnumeric():
+        print("Error: Please enter letter (A-J) for row and (0-9) for column")
+        continue
+    row = alphabet.find(row)
+    if not (-1 < row < grid_size):
+        print("Error: Please enter letter (A-J) for row and (0-9) for column")
+        continue
+    col = int(col)
+    if not (-1 < col < grid_size):
+        print("Error: Please enter letter (A-J) for row and (0-9) for column")
+        continue
+    if grid[row][col] == "#" or grid[row][col] == "X":
+        print("You have already shot a bullet here, pick somewhere else")
+        continue
+    if grid[row][col] == "." or grid[row][col] == "O":
+        valid_placement = True
+
+return row, col
 
 def check_for_ship_sunk(row, col):
-            #if/ when all parts of the ship is shot, ships sunk will be incremented
-            global ship_positions
-            global grid
+    #if/ when all parts of the ship is shot, ships sunk will be incremented
+    global ship_positions
+    global grid
 
-            for position in ship_positions:
-                start_row = position[0]
-                end_row = position[1]
-                start_col = position[2]
-                end_col = position[3]
-                if start_row <= row <= end_row and start_col <= col <= end_col:
-                    #from above code ship is found, check to see if it is all sunk now.
-                    for r in range(start_row, end_row):
-                        for c in range(start_col, end_col):
-                            if grid[r][c] != "X":
-                                return False
-            return True
-        
- def shoot_bullet():
-            #updates grid and ships based on where the bullet was shot.
-            global grid
-            global sunk_ships
-            global bullets_left
+    for position in ship_positions:
+        start_row = position[0]
+        end_row = position[1]
+        start_col = position[2]
+        end_col = position[3]
+        if start_row <= row <= end_row and start_col <= col <= end_col:
+            #from above code ship is found, check to see if it is all sunk now.
+            for r in range(start_row, end_row):
+                for c in range(start_col, end_col):
+                    if grid[r][c] != "X":
+                        return False
+    return True
 
-            row, col = valid_bullet_placement()
-            print("")
-            print("--------------------")
-
-            if grid[row][col] == ".":
-                print("You missed, no ship was shot")
-                grid[row][col] = "#"
-            elif grid[row][col] == "O":
-                print("You hit!", end=" ")
-                grid[row][col] = "X"
-                if check_for_ship_sunk(row, col):
-                    print("A ship was completely sunk!")
-                    sunk_ships += 1
-                else:
-                    print("A ship was shot!")
-            
-            bullets_left -= 1
-        
-        
-def check_game_over():
-    #when users shots = 0 or all ships are sunk the game is over, heres the function to implement this.
+def shoot_bullet():
+    #updates grid and ships based on where the bullet was shot.
+    global grid
     global sunk_ships
-    global num_ships
     global bullets_left
-    global game_over
 
-    if num_ships == sunk_ships:
-        print("Congradulations, you won!")
-        game_over = True
-    elif bullets_left <= 0:
-        print("Sorry, you lost! You ran out of bullets, try again!")
-        game_over = True
+    row, col = valid_bullet_placement()
+    print("")
+    print("--------------------")
+
+    if grid[row][col] == ".":
+        print("You missed, no ship was shot")
+        grid[row][col] = "#"
+    elif grid[row][col] == "O":
+        print("You hit!", end=" ")
+        grid[row][col] = "X"
+        if check_for_ship_sunk(row, col):
+            print("A ship was completely sunk!")
+            sunk_ships += 1
+        else:
+            print("A ship was shot!")
+    
+    bullets_left -= 1
+
+
+def check_game_over():
+#when users shots = 0 or all ships are sunk the game is over, heres the function to implement this.
+global sunk_ships
+global num_ships
+global bullets_left
+global game_over
+
+if num_ships == sunk_ships:
+print("Congradulations, you won!")
+game_over = True
+elif bullets_left <= 0:
+print("Sorry, you lost! You ran out of bullets, try again!")
+game_over = True
 
 def main():
-    #runs the game loop, pulls it all together.
-    global game_over
+#runs the game loop, pulls it all together.
+global game_over
 
-    print("-----Welcome to Battleships-----")
-    print("You have 50 bullets to take down 8 ships, let the battle begin!")
+print("-----Welcome to Battleships-----")
+print("You have 50 bullets to take down 8 ships, let the battle begin!")
 
-    create_grid()
+create_grid()
 
-    while game_over is False:
-        print_grid()
-        print("Number of ships remaining: " + str(num_ships-sunk ships))
-        print("Number of bullet left: " + str(bullets_left))
-        shoot_bullet()
-        print("--------------------")
-        print("")
-        check_game_over()
+while game_over is False:
+print_grid()
+print("Number of ships remaining: " + str(num_ships-sunk ships))
+print("Number of bullet left: " + str(bullets_left))
+shoot_bullet()
+print("--------------------")
+print("")
+check_game_over()
 
 if __name__ == '__main__':
-    #this will only be called when program is run from terminal or an IDE
-    main()
-                
+#this will only be called when program is run from terminal or an IDE
+main()
+        
